1b8222996efddb61d0995fac66830ca7b4caa9e3
john praneeth <johnpraneeth3030@gmail.com>
2025-07-20 09:14:08 +0530

    inital commit

---
+++ JP_learning/wsgi.py
@@ -0,0 +1,5 @@
+# EduStream/wsgi.py
+import os
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JP_learning.settings')
+application = get_wsgi_application()
